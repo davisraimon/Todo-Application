e8f56fcd73b02a55a57aeb7540c9216580cabcb3
Davis Raimon <davisraimon@gmail.com>
2020-09-13 00:13:37 +0530

    minor changes commit

--- server.py
+++ server.py
@@ -1,31 +1,43 @@
 from flask import Flask,request
 import json
-from dbconnector import dbconnect
 from bson import ObjectId,json_util
+from dbconnector import dbconnect
+
 
 app = Flask(__name__,static_folder='./build', static_url_path='/')
 collection = dbconnect()
 
+# Serving static build files
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+# @desc   : Get all the todo items
+# @returns: JSON array
 @app.route('/get-todo',methods=['GET'])
 def get_todo():   
     res = collection.find({}).sort([['_id',-1]])
     return json_util.dumps(res)
 
+# @desc   : Get the todo item by id
+# @params : <int:id>
+# @returns: JSON array
 @app.route('/get-todo/<id>',methods=['GET'])
 def get_todo_id(id):
     res = collection.find({"_id":ObjectId(id)})             
     return json_util.dumps(res) 
 
+# @desc   : Add new todo item
+# @returns: JSON array
 @app.route('/add-todo',methods=['POST'])
 def add_todo():   
     collection.insert({"bucket":request.json['bucket'], "title":request.json['title'],"completed":request.json['completed']})         
     res = collection.find({}).sort([['_id',-1]])
     return json_util.dumps(res)
 
+# @desc   : Delete the todo item by id
+# @params : <int:id>
+# @returns: JSON array
 @app.route('/delete-todo/<id>',methods=['DELETE'])
 def delete_todo(id):
     deleted = collection.delete_one({"_id":ObjectId(id)})  
@@ -33,6 +45,9 @@ def delete_todo(id):
     res = collection.find({}).sort([['_id',-1]])
     return json_util.dumps(res)
 
+# @desc   : Edit the todo item by id
+# @params : <int:id>
+# @returns: JSON array
 @app.route('/edit-todo/<id>',methods=['POST'])
 def edit_todo(id):
     res = collection.find({"_id":ObjectId(id)})    
