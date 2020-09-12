e6735656b911591577a1006d6b8be7836e691bda
Davis Raimon <davisraimon@gmail.com>
2020-09-12 20:24:33 +0530

    backend initial commit

---
+++ dbconnector.py
@@ -0,0 +1,10 @@
+from flask_pymongo import pymongo
+import os
+con_string = os.environ.get('MONGODB_URL')
+
+def dbconnect():    
+    client = pymongo.MongoClient(con_string)
+    db = client.get_database('test')
+    collection = db.get_collection('todo')
+    print("------DB Connected------")
+    return collection
---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask,request
+import json
+from dbconnector import dbconnect
+from bson import ObjectId,json_util
+
+app = Flask(__name__)
+collection = dbconnect()
+
+@app.route('/get-todo',methods=['GET'])
+def get_todo():   
+    res = collection.find({}).sort([['_id',-1]])
+    return json_util.dumps(res)
+
+@app.route('/get-todo/<id>',methods=['GET'])
+def get_todo_id(id):
+    res = collection.find({"_id":ObjectId(id)})             
+    return json_util.dumps(res) 
+
+@app.route('/add-todo',methods=['POST'])
+def add_todo():   
+    collection.insert({"bucket":request.json['bucket'], "title":request.json['title'],"completed":request.json['completed']})         
+    res = collection.find({}).sort([['_id',-1]])
+    return json_util.dumps(res)
+
+@app.route('/delete-todo/<id>',methods=['DELETE'])
+def delete_todo(id):
+    deleted = collection.delete_one({"_id":ObjectId(id)})  
+    print(deleted.deleted_count) 
+    res = collection.find({}).sort([['_id',-1]])
+    return json_util.dumps(res)
+
+@app.route('/edit-todo/<id>',methods=['POST'])
+def edit_todo(id):
+    res = collection.find({"_id":ObjectId(id)})    
+    collection.update({"_id":ObjectId(id)},{"bucket":request.json['bucket'], "title":request.json['title'],"completed":request.json['completed']})         
+    res = collection.find({}).sort([['_id',-1]])
+    return json_util.dumps(res)   
+      
+if __name__ == "__main__":
+    app.run()    
