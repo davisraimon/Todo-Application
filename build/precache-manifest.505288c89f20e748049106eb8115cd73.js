18700b472454d05105b90c515e0f3f29cdb82cb0
Davis Raimon <davisraimon@gmail.com>
2020-09-12 20:43:18 +0530

    build folder commit

---
+++ build/precache-manifest.505288c89f20e748049106eb8115cd73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758173524725e9638b67c4426e8b6f73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b69614a6467e4d07447",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "936072fa5202ca906e88",
+    "url": "/static/css/main.9dbcde19.chunk.css"
+  },
+  {
+    "revision": "0b69614a6467e4d07447",
+    "url": "/static/js/2.67c62f6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.67c62f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936072fa5202ca906e88",
+    "url": "/static/js/main.32647fd5.chunk.js"
+  },
+  {
+    "revision": "fe39c6edd69c85b8ea65",
+    "url": "/static/js/runtime-main.5fa3d7bf.js"
+  }
+]);
