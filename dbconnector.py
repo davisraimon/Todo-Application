569d7c86e4f66aa3d4d21c10a323d9486d26eec6
Davis Raimon <davisraimon@gmail.com>
2020-09-13 12:39:10 +0530

    updates commit

--- dbconnector.py
+++ dbconnector.py
@@ -2,6 +2,8 @@ import os
 from flask_pymongo import pymongo
 con_string = os.environ.get('MONGODB_URL',None)
 
+# @desc   : Connect with MongoDB and returns collection
+# @returns: Returns the collection
 def dbconnect():    
     client = pymongo.MongoClient(con_string)
     db = client.get_database('test')
